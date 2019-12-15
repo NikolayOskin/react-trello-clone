4661184a8ea6ce60be495941b0836e82da76c6c0
Oskin <wskeal@gmail.com>
2019-12-16 01:28:29 +0300

    refactor: rewriting Column to functional component (with hooks)

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,86 +1,59 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Card from "./Card";
 import NewButton from "./ui/NewButton";
 import SubmitButton from "./ui/Form/SubmitButton";
 import CloseButton from "./ui/Form/CloseButton";
 import { Droppable } from 'react-beautiful-dnd';
 
+const Column = (props) => {
+    const [cardFormShowed, setCardFormShowed] = useState(false)
+    const [cardText, setCardText] = useState('')
 
-class Column extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cardFormShowed: false,
-            cardText: ''
+    const addCard = () => {
+        if (cardText.trim().length > 0) {
+            props.onCardAdded(props.index, cardText.trim())
+            setCardFormShowed(false)
+            setCardText('')
         }
-        this.showCardForm = this.showCardForm.bind(this)
-        this.hideCardForm = this.hideCardForm.bind(this)
-        this.addCard = this.addCard.bind(this)
-        this.changeCardText = this.changeCardText.bind(this)
     }
 
-    showCardForm() {
-        this.setState({cardFormShowed: true})
-    }
+    return (
+        <div className="column">
+            <h2 className="column__title">{props.column.name}</h2>
 
-    hideCardForm() {
-        this.setState({cardFormShowed: false})
-    }
-
-    addCard() {
-        if (this.state.cardText.trim().length > 0) {
-            this.props.onCardAdded(this.props.index, this.state.cardText.trim())
-            this.setState({
-                cardFormShowed: false,
-                cardText: ''
-            })
-        }
-
-    }
-
-    changeCardText(event) {
-        this.setState({cardText: event.target.value})
-    }
-
-    render() {
-        return (
-            <div className="column">
-                <h2 className="column__title">{this.props.column.name}</h2>
-
-                <Droppable droppableId={"droppable-" + this.props.index}>
-                    {(provided, snapshot) => (
-                        <div
-                            className="cards"
-                            ref={provided.innerRef}
-                            {...provided.droppableProps}
-                        >
-                            {this.props.column.items.map((card, i) => <Card key={i} index={i} column={this.props.index} card={card}/>)}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-
-
-                <form action=""
-                      className={"form form--card " + (this.state.cardFormShowed === false ? 'hidden' : '')}>
-                    <textarea className="form__input"
-                              rows="3"
-                              placeholder="Введите название карточки"
-                              value={this.state.cardText}
-                              onChange={this.changeCardText}
-                    />
-                    <div className="form__buttons">
-                        <SubmitButton onSubmited={this.addCard} text="Добавить карточку"/>
-                        <CloseButton onClicked={this.hideCardForm} />
+            <Droppable droppableId={"droppable-" + props.index}>
+                {(provided, snapshot) => (
+                    <div
+                        className="cards"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        {props.column.items.map((card, i) => <Card key={i} index={i} column={props.index} card={card}/>)}
+                        {provided.placeholder}
                     </div>
-                </form>
+                )}
+            </Droppable>
 
-                {!this.state.cardFormShowed &&
-                <NewButton text="Добавить еще одну карточку" onClicked={this.showCardForm}/>}
+            {cardFormShowed &&
+            <form action="" className="form form--card">
+                <textarea className="form__input"
+                          rows="3"
+                          placeholder="Введите название карточки"
+                          value={cardText}
+                          onChange={(e) => setCardText(e.target.value)}
+                />
+                <div className="form__buttons">
+                    <SubmitButton onSubmited={addCard} text="Добавить карточку"/>
+                    <CloseButton onClicked={() => setCardFormShowed(false)}/>
+                </div>
+            </form>
+            }
 
-            </div>
-        );
-    }
+            {!cardFormShowed &&
+            <NewButton text="Добавить еще одну карточку" onClicked={() => setCardFormShowed(true)}/>}
+
+        </div>
+    )
 }
 
 export default Column;

061d6a5927d515be583d94c6682c8ade95af87d4
Oskin <wskeal@gmail.com>
2019-12-17 14:51:48 +0300

    feature: rotate dragged item

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 
 const Card = ({ index, column, card}) => {
+    const getItemStyle = (isDragging, { transform, ...draggableStyles }) => ({
+        userSelect: 'none',
+        transform: isDragging ? (transform + "rotate(5deg)") : transform,
+        ...draggableStyles
+    });
+
     return (
         <Draggable
             key={index}
@@ -13,6 +19,10 @@ const Card = ({ index, column, card}) => {
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
+                    style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                    )}
                 >
                     {card}
                 </div>
